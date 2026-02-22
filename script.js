13746b05fc9b74d3c9d9ef1615a0217856d1979f
Loyoussef <67337210+Loyoussef@users.noreply.github.com>
2026-02-22 17:59:51 +0000

    Refactor script to Class instead of constructor as well as seprate clas and ui handling

--- script.js
+++ script.js
@@ -1,97 +1,74 @@
 const library = [];
-const data = [];
-
-function Book(id, title, author, pages, read) {
-  this.id = id;
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+    
+class Book{
+    
+    constructor(id, title, author, pages, read) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
-
-Book.prototype.toggleRead = function( ) {
-  this.read = this.read === "No" ? "Yes" : "No";
-};
-
-function bookCreate(title,author,pages,read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-   
-    id= crypto.randomUUID()
+  function createBook(book){
+       
+        const container = document.getElementById("cards");
     
+        const div = document.createElement("div");
+        div.classList.add("card");
+        div.id = `book-${book.id}`;
 
-    const bookid = new Book(id,this.title,this.author,this.pages,this.read); 
-    
-    library.push(bookid);
-    data.push(bookid);
-    getBooks(bookid);
-}
+        const title = document.createElement("h3");
+        title.textContent = book.title;
 
+        const author = document.createElement("p");
+        author.textContent = `Author: ${book.author}`;
 
-function getBooks(item){
-  const container = document.getElementById("cards");
+        const pages = document.createElement("p");
+        pages.textContent = `Pages: ${book.pages}`; 
 
-  const div = document.createElement("div");
-  div.classList.add("card");
-  div.id = `book-${item.id}`;
-
-  const title = document.createElement("h3");
-  title.textContent = item.title;
-
-  const author = document.createElement("p");
-  author.textContent = `Author: ${item.author}`;
-
-  const pages = document.createElement("p");
-  pages.textContent = `Pages: ${item.pages}`; 
-
-  const read = document.createElement("p");
-  read.textContent = `Read: ${item.read}`; 
+        const read = document.createElement("p");
+        read.textContent = `Read: ${book.read ? "Yes" : "No"}`;
   
-  const remove = document.createElement("button");
-  remove.textContent = "Remove";
-  remove.addEventListener("click", () => {
-   const card = document.getElementById(`book-${item.id}`);
-   card.remove();
-  });
+        const remove = document.createElement("button");
+        remove.textContent = "Remove";
+        remove.addEventListener("click", () => {
+        const card = document.getElementById(`book-${book.id}`);
+        card.remove();
+        library.splice(library.indexOf(book), 1);
+        });
 
-  const readButton = document.createElement("button");
-  readButton.textContent = "Read";
-  readButton.addEventListener("click", () => {
-  item.toggleRead();                 
-  read.textContent = `Read: ${item.read}`;
-});
+        const readButton = document.createElement("button");
+        readButton.textContent = "Read";
+        readButton.addEventListener("click", () => {
+        book.toggleRead();
+        read.textContent = `Read: ${book.read ? "Yes" : "No"}`;  
+        });
 
+        div.append(title, author, pages, read, remove, readButton);
+        container.appendChild(div);
+    }
 
-  div.append(title, author, pages, read, remove, readButton);
-  container.appendChild(div);
-
-}
 
 const addBook = document.getElementById("addBook");
 addBook.addEventListener("click", () => {
-  const title = prompt("Enter the title of the book:");
-  const author = prompt("Enter the author of the book:");
-  const pages = prompt("Enter the number of pages:");
-  var read = prompt("Have you read the book?");
-  if (read === 'Yes') {
-  read = 'Yes'
-} else {
-  read = 'No'
-}
-  bookCreate(title, author, pages, read);
+    const title = prompt("Enter the title of the book:");
+    const author = prompt("Enter the author of the book:");
+    const pages = prompt("Enter the number of pages:");
+    var read = prompt("Have you read the book?");
+    if (read === 'Yes') {
+        read = true;
+    } else if (read === 'No') {
+        read = false;
+    }
+
+    let id = crypto.randomUUID();
+    const bookid = new Book(id,title,author,pages,read); 
+    library.push(bookid);
+    createBook(bookid);
 });
-
-
-
-
-
-
-  
-
-
-
-
-
